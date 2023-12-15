28e8903a8927fdd9c2abdb854a2f2e3e821322ad
Renzo Nicolas Ahumada <94655050+RenzoAhumada@users.noreply.github.com>
2023-12-14 21:46:05 -0300

    Pagina ya realizada y creada con exito

--- Seguros/procesar_formulario.php
+++ Seguros/procesar_formulario.php
@@ -4,8 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefono = $_POST["telefono"];
     $email = $_POST["email"];
     $consulta = $_POST["consulta"];
-    
-    //Configuracion del correo
+
+    // Configura el destinatario y el asunto del correo
     $to = "claudiaalecruz@yahoo.com.ar";
     $subject = "Nueva consulta de contacto";
 
